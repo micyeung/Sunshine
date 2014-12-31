e3b8bef287ce13c0ae244712bf81be506562d6bb
micyeung <micgoyeung@gmail.com>
2014-12-31 15:09:54 +0800

    Bug fix for Calendar month off-by-one bug

--- app/src/main/java/com/example/micyeung/sunshine/app/Utility.java
+++ app/src/main/java/com/example/micyeung/sunshine/app/Utility.java
@@ -269,7 +269,22 @@ public class Utility {
     }
 
     public static int getMonth(String dateStr) {
-        return Integer.parseInt(dateStr.substring(4,6));
+        int monthNum = Integer.parseInt(dateStr.substring(4,6));
+        switch(monthNum) {
+            case 1 : return Calendar.JANUARY;
+            case 2 : return Calendar.FEBRUARY;
+            case 3 : return Calendar.MARCH;
+            case 4 : return Calendar.APRIL;
+            case 5 : return Calendar.MAY;
+            case 6 : return Calendar.JUNE;
+            case 7 : return Calendar.JULY;
+            case 8 : return Calendar.AUGUST;
+            case 9 : return Calendar.SEPTEMBER;
+            case 10 : return Calendar.OCTOBER;
+            case 11 : return Calendar.NOVEMBER;
+            case 12 : return Calendar.DECEMBER;
+        }
+        return -1;
     }
 
     public static int getDay(String dateStr) {
