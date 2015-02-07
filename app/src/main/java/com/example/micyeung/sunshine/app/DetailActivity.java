923993dc3a80c3f6c3a9fb94a619cd73f10bc910
micyeung <micgoyeung@gmail.com>
2015-02-07 15:08:08 +0800

    Make Up button act like Back button

--- app/src/main/java/com/example/micyeung/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/micyeung/sunshine/app/DetailActivity.java
@@ -53,6 +53,9 @@ public class DetailActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent launchSettingsIntent = new Intent(this,SettingsActivity.class);
             startActivity(launchSettingsIntent);
+        } else if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
